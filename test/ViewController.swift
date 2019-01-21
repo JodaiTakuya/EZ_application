1f3e3ecf053dbb47c7bfa4c7321ce4fbaee95960
Takuya Jodai <JodaiTakuya@users.noreply.github.com>
2019-01-21 20:19:01 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Jodai Takuya on 2019/01/21.
+//  Copyright © 2019 城代拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
